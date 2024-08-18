0ad8d529a057e37d41730938fac90017132cc3b0
AhnJiHoon <jihoon5535@gmail.com>
2024-08-18 16:17:55 +0900

    초기 셋팅

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </HydrationBoundary>
+    </QueryClientProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        홈 화면입니다.
+      </div>
+    </main>
+  );
+}
